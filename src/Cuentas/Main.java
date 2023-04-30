b2e481a1e6a420c64b10d31ec95d850230fc8f4b
Ithaisa Sánchez González <ithaisasanchezgonzalez@gmail.com>
2023-04-30 13:29:31 +0100

    first commit

---
+++ src/Cuentas/Main.java
@@ -0,0 +1,36 @@
+package Cuentas;
+
+public class Main {
+
+    public static class Cantidad{
+        public float[]args;
+
+        public Cantidad(float[]args){
+            this.args = args;
+        }
+    }
+    public static void main(String[] args) {
+        operativas_cuentas();
+    }
+
+    private static void operativas_cuentas() {
+        CCuenta miCuenta;
+        double saldoActual;
+
+        miCuenta = new CCuenta("Antonio López","1000-2365-85-1230456789",2500,0);
+        saldoActual = miCuenta.estado();
+        System.out.println("El saldo actual es"+ saldoActual );
+
+        try {
+            miCuenta.retirar(2300);
+        } catch (Exception e) {
+            System.out.print("Fallo al retirar");
+        }
+        try {
+            System.out.println("Ingreso en cuenta");
+            miCuenta.ingresar(695);
+        } catch (Exception e) {
+            System.out.print("Fallo al ingresar");
+        }
+    }
+}
